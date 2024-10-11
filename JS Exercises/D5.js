65664afbb2744550f36c67c3612dd3602b28fe94
GiuseppeDot <giuseppegianni190@gmail.com>
2024-10-11 15:51:42 +0200

    aggiornamento 2

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -74,7 +74,7 @@ const justTrims = [];
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
-while (numericArray <= 32) {
+while (numericArray === 32) {
   numericArray++;
 }
 console.log(numericArray);
@@ -85,3 +85,92 @@ console.log(numericArray);
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+const alphbetePositions = [];
+let position;
+charactersArray.forEach(character >= position);
+
+switch (character) {
+  case "a":
+    position = 1;
+    break;
+  case "b":
+    position = 2;
+    break;
+  case "c":
+    position = 3;
+    break;
+  case "d":
+    position = 4;
+    break;
+  case "e":
+    position = 5;
+    break;
+  case "f":
+    position = 6;
+    break;
+  case "g":
+    position = 7;
+    break;
+  case "h":
+    position = 8;
+    break;
+  case "i":
+    position = 9;
+    break;
+  case "j":
+    position = 10;
+    break;
+  case "k":
+    position = 11;
+    break;
+  case "l":
+    position = 12;
+    break;
+  case "m":
+    position = 13;
+    break;
+  case "n":
+    position = 14;
+    break;
+  case "o":
+    position = 15;
+    break;
+  case "p":
+    position = 16;
+    break;
+  case "q":
+    position = 17;
+    break;
+  case "r":
+    position = 18;
+    break;
+  case "s":
+    position = 19;
+    break;
+  case "t":
+    position = 20;
+    break;
+  case "u":
+    position = 21;
+    break;
+  case "v":
+    position = 22;
+    break;
+  case "w":
+    position = 23;
+    break;
+  case "x":
+    position = 24;
+    break;
+  case "y":
+    position = 25;
+    break;
+  case "z":
+    position = 26;
+    break;
+  default:
+    position = null;
+}
+alphbetePositions.push(position);
+console.log(alphbetePositions);
